b2f50dfeb41858d107389d7c18e141b498509a56
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-05-22 17:55:30 +0530

    Create instruction.txt

---
+++ instruction.php
@@ -0,0 +1,6 @@
+#instruction Page 
+  1) first create the one folder :- email  
+    then open the folder and go to the inside the folder And apply the second Step
+  2)-> run the command  in your Command Line
+      ->composer require phpmailer/phpmailer
+  3) use index.php page
