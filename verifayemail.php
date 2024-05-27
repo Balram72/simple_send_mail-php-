c8aaf9828905f5bc63cabeaccf84b0469c183b98
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-05-27 14:43:28 +0530

    Update verifayemail.php

--- verifayemail.php
+++ verifayemail.php
@@ -2,21 +2,21 @@
 require './vendor/autoload.php'; 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-  $email = 'balrampanda87@gmail.com';
+  $email = 'balrampanda11@gmail.com';
     try {
         $mail = new PHPMailer(true);
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'balrampanda77@gmail.com'; // Your Gmail SMTP username
-        $mail->Password = 'aaahyatjerqcxxgc'; // Your Gmail SMTP password
+        $mail->Username = 'Your Gmail'; // Your Gmail SMTP username
+        $mail->Password = 'Your Gmail Password'; // Your Gmail SMTP password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
-        $mail->setFrom('balrampanda77@gmail.com', 'MVDO Recruitment Registration');
+        $mail->setFrom('Your Gmail', 'Recruitment Registration');
         $mail->addAddress($email);
         $mail->isHTML(true);
         $mail->CharSet = 'UTF-8';
-        $mail->Subject = 'Mvdo Recruitment Registration';
+        $mail->Subject = 'Recruitment Registration';
         $mail->Body = 'hello';
         $mail->send();
          
